2367c834fa22487dc31f445de7325597c7e54f21
karimabd84 <abdelkarim.esti@gmail.com>
2017-02-15 16:46:31 +0100

    bundles added

--- STIFE.Web/App_Start/BundleConfig.cs
+++ STIFE.Web/App_Start/BundleConfig.cs
@@ -7,16 +7,12 @@ namespace STIFE.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //    "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-            //    "~/Scripts/jquery.unobtrusive*",
-            //    "~/Scripts/jquery.validate*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-            //    "~/Scripts/knockout-{version}.js",
-            //    "~/Scripts/knockout.validation.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.unobtrusive*",
+                "~/Scripts/jquery.validate*"));
 
             //bundles.Add(new ScriptBundle("~/bundles/app").Include(
             //    "~/Scripts/sammy-{version}.js",
@@ -26,18 +22,18 @@ namespace STIFE.Web
             //    "~/Scripts/app/home.viewmodel.js",
             //    "~/Scripts/app/_run.js"));
 
-            //// Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
-            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-            //    "~/Scripts/modernizr-*"));
+            // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
 
-            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-            //    "~/Scripts/bootstrap.js",
-            //    "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
 
-            //bundles.Add(new StyleBundle("~/Content/css").Include(
-            //     "~/Content/bootstrap.css",
-            //     "~/Content/Site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/bootstrap.css",
+                 "~/Content/Site.css"));
 
 #if DEBUG
             BundleTable.EnableOptimizations = false;
